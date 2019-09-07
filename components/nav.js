626dde828dd0269d3622c6e664559d7e1f5c3da2
khang8591 <khang859@gmail.com>
2019-09-07 09:56:57 -0400

    added my image

--- components/nav.js
+++ components/nav.js
@@ -1,105 +1,83 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
-const Nav = () => {
-  const [isMobile, setIsMobile] = useState(true);
+export const MobileNav = () => (
+  <div className="nav-bar">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <a className="navbar-brand" href="#">
+        KN
+      </a>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="info-nav">
+          <img src="/static/nav-image.jpg" className="photo-nav" />
+          <p>
+            Hi, my name is Khang Nguyen and im a Senior Developer, welcome to my
+            personal site.
+          </p>
+        </div>
+        <hr />
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <Link href="/">
+              <a className="nav-link" href="#">
+                About me
+              </a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Porfolio
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Blog
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <style jsx>{`
 
-  useEffect(() => {
-    function handleResize() {
-      if (window.innerWidth < 768) {
-        setIsMobile(true);
-      } else {
-        setIsMobile(false);
+      hr {
+        color: white;
+        border-color: white;
       }
-    }
 
-    window.addEventListener("resize", handleResize);
-    window.addEventListener("load", handleResize);
+      .navbar-dark .navbar-brand {
+        font-family: "Segoe UI Bold";
+      }
 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-      window.removeEventListener("load", handleResize);
-    };
-  }, []);
+      .photo-nav {
+        border-radius: 50%;
+        margin: 0 auto;
+        max-width: 150px;
+      }
+      .info-nav {
+        margin-top: 20px;
+        margin-left: auto;
+        margin-right: auto;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        color: white;
+      }
 
-  return (
-    <div className="nav-bar">
-      {isMobile ? (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-          <span className="navbar-toggler-icon"></span>
-          </button>
-          <a className="navbar-brand" href="#">KN</a>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <div className="info-nav">
-              <img src="https://picsum.photos/150/150" className="photo-nav"/>
-              <p>Hi, my name is Khang Nguyen and im a Senior Developer, welcome to my personal site.</p>
-            </div>
-            <hr />
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">
-                  About me
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Porfolio
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Blog
-                </a>
-              </li>
-            </ul>
-            
-          </div>
-        </nav>
-      ) : (
-        ""
-      )}
-
-      <style jsx>{`
-        hr{
-          color: white;
-          border-color: white;
-        }
-
-        .navbar-dark .navbar-brand{
-          font-family: 'Segoe UI Bold';
-        }
-
-        .photo-nav{
-          border-radius: 50%;
-          margin: 0 auto;
-          max-width: 150px;
-        }
-        .info-nav{
-          margin-top: 20px;
-          margin-left: auto;
-          margin-right: auto;
-          display:flex;
-          flex-direction: column;
-          justify-content: center;
-          color: white;
-        }
-
-        .info-nav p {
-          margin-top: 20px;
-          text-align:center;
-        }
-      `}</style>
-    </div>
-  );
-};
-
-export default Nav;
+      .info-nav p {
+        margin-top: 20px;
+        text-align: center;
+      }
+    `}</style>
+  </div>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../components/layout";
 import Banner from "../components/banner";
-import Nav from "../components/nav";
+import { MobileNav } from "../components/nav";
 
-const Home = () => (
-  <Layout>
-    <div className="container-wrapper">
-      <div className="container-fluid no-gutter">
-        <Nav />
-        <Banner />
+const Home = () => {
+  const [isMobile, setIsMobile] = useState(true);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth < 768) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    }
+
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("load", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("load", handleResize);
+    };
+  }, []);
+  return (
+    <Layout>
+      <div className="container-wrapper">
+        <div className="container-fluid no-gutter">
+          {isMobile ? <MobileNav /> : ""}
+          <Banner />
+        </div>
       </div>
-    </div>
-    <style jsx>{`
-      .container-wrapper{
-        margin: 0;
-        padding: 0;
-        font-family: "Segoe UI Regular";
-      }
-      .col-10{
-        min-height: 400px;
-      }
+      <style jsx>{`
+        .container-wrapper {
+          margin: 0;
+          padding: 0;
+          font-family: "Segoe UI Regular";
+        }
+        .col-10 {
+          min-height: 400px;
+        }
 
-      .no-gutter{
-        margin: 0;
-        padding: 0;
-      }
-    `}</style>
+        .no-gutter {
+          margin: 0;
+          padding: 0;
+        }
+      `}</style>
     </Layout>
-);
-
+  );
+};
 export default Home;
