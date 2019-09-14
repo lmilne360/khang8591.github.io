388a83f357f34f27f93273f1a63fd5555783e8b0
khang8591 <khang859@gmail.com>
2019-09-13 22:12:13 -0400

    fontawesome

--- components/layout.js
+++ components/layout.js
@@ -6,8 +6,8 @@ const Layout = props => (
       <title>Khang Nguyen</title>
       <link rel="shortcut icon" href="/static/logo.ico" />
       <link rel="stylesheet" href="/static/fonts/segoe-ui/style.css" />
+      <link rel="stylesheet" href="/static/fonts/fontawesome/fontawesome.min.css" />
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"/>
-      <script src="https://kit.fontawesome.com/d9207a4fe7.js"></script>
     </Head>
     {props.children}
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
